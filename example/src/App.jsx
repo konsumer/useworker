9f005d59915ed56a62d12ccfdf09201431168ed7
David Konsumer <konsumer@jetboystudio.com>
2024-06-28 21:07:49 -0700

    move icon for simpler example

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,22 +1,6 @@
 import { useState } from 'react'
 import useResizeImage from './useResizeImage.js'
-
-const IconError = (props) => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    className='h-6 w-6 shrink-0 stroke-current'
-    fill='none'
-    viewBox='0 0 24 24'
-    {...props}
-  >
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      strokeWidth={2}
-      d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
-    />
-  </svg>
-)
+import IconError from './IconError.jsx'
 
 export default function PageExample () {
   const [url, setUrl] = useState()
