162b15bed0f8ee0bc592fc0c6369c9c6ae4b89cb
David Konsumer <83857+konsumer@users.noreply.github.com>
2024-07-02 15:14:06 -0700

    check url before revoking

--- index.js
+++ index.js
@@ -3,19 +3,22 @@
 import { useRef, useEffect } from 'react'
 
 const noop = () => {}
-export default function useWorker ({ onWork = noop, onFinish = noop, onError = noop }, deps = [], options={ type: 'module' }) {
+export default function useWorker ({ onWork = noop, onFinish = noop, onError = noop }, deps = [], options = { type: 'module' }) {
   const worker = useRef()
+  let url
   useEffect(() => {
     if (onWork instanceof Worker) {
       worker.current = onWork
     } else {
-      const url = window.URL.createObjectURL(new Blob([`self.onmessage = ${onWork.toString()}`], { type: 'text/javascript' }))
+      url = window.URL.createObjectURL(new Blob([`self.onmessage = ${onWork.toString()}`], { type: 'text/javascript' }))
       worker.current = new Worker(url, options)
     }
     worker.current.onmessage = e => onFinish(e.data)
     worker.current.onerror = onError
     return () => {
-      URL.revokeObjectURL(url)
+      if (url && url.startsWith('blob:')) {
+        URL.revokeObjectURL(url)
+      }
       if (worker.current) {
         worker.current.terminate()
       }
